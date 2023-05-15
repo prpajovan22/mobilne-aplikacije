89e8f815c40c3bc052ff889a13b49a75f820b377
prpajovan22 <prpajovan22@gmail.com>
2023-05-15 22:14:32 +0200

    Added activitys

---
+++ Slagalica/app/src/main/java/com/ftn/slagalica/KoZnaZnaActivity.java
@@ -0,0 +1,14 @@
+package com.ftn.slagalica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KoZnaZnaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ko_zna_zna);
+    }
+}
---
+++ Slagalica/app/src/main/java/com/ftn/slagalica/SpojniceActivity.java
@@ -0,0 +1,14 @@
+package com.ftn.slagalica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpojniceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spojnice);
+    }
+}
