cabd594551b3cbe2c182d5f95d13b6127a35d3c5
prpajovan22 <prpajovan22@gmail.com>
2023-05-15 22:43:44 +0200

    Added to main

--- Slagalica/app/src/main/java/com/ftn/slagalica/KoZnaZnaActivity.java
+++ Slagalica/app/src/main/java/com/ftn/slagalica/KoZnaZnaActivity.java
@@ -2,7 +2,10 @@ package com.ftn.slagalica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class KoZnaZnaActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class KoZnaZnaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ko_zna_zna);
+        Button button1 = findViewById(R.id.odustani);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(KoZnaZnaActivity.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
