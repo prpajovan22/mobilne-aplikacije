7785efbbe03d75587c547b198bab5e59aa6e906f
prpajovan22 <prpajovan22@gmail.com>
2023-05-22 22:20:35 +0200

    Added model

--- Slagalica/app/src/main/java/com/ftn/slagalica/MainActivity.java
+++ Slagalica/app/src/main/java/com/ftn/slagalica/MainActivity.java
@@ -2,7 +2,10 @@ package com.ftn.slagalica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button profil = findViewById(R.id.profile);
+        Button start = findViewById(R.id.start);
+
+        profil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, KoZnaZnaActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
